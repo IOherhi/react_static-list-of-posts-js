6e88b034e7ab45230832c58dee3508d63c5d1f10
Ivan Herhi <ionherhi@gmail.com>
2025-04-07 12:46:18 +0200

    version01

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,24 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo';
+
+export function PostInfo({ post, user }) {
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          {user ? <UserInfo key={user.id} user={user} /> : null}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    </div>
+  );
+}
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,12 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostList = ({ posts, users, comments }) => (
+  <div className="PostList">
+    {posts.map((post, i) => (
+      <PostInfo key={post.id} post={post} user={users[i]} />
+    ))}
+
+    <CommentList comments={comments} />
+  </div>
+);
